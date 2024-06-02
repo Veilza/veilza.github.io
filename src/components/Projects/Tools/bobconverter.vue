3b34646bc2813f8e0c54e04c3ddff8c32916cfb0
Veilza <veilzakinsemi@gmail.com>
2024-06-02 10:59:26 -0500

    Add border to bobconverter and move secondary color to global.css

--- src/components/Projects/Tools/bobconverter.vue
+++ src/components/Projects/Tools/bobconverter.vue
@@ -118,6 +118,7 @@
 <style module>
   .container {
     background-color: var(--primary-color);
+    border: 2px inset var(--secondary-color);
     padding: 20px;
     border-radius: 10px;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
