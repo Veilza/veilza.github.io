6b410de9190cc4be1fafeeb41b466c2a9c11a903
Veilza <veilzakinsemi@gmail.com>
2024-06-30 13:26:26 -0500

    Update projects page

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -1,7 +1,15 @@
+import WOD5EBanner from './../../assets/wod5eBanner.webp'
+import SettlementSheetsBanner from './../../assets/SettlementSheetsBanner.webp'
+import ShowTokenArtBanner from './../../assets/ShowTokenArtBanner.webp'
+
 // Logic
 export default {
     name: 'ProjectComponent',
     data() {
-        return { }
+        return {
+            WOD5EBanner,
+            SettlementSheetsBanner,
+            ShowTokenArtBanner
+        }
     }
 }
--- src/components/Projects/projects.vue
+++ src/components/Projects/projects.vue
@@ -1,7 +1,26 @@
 <template>
-    <div :class="$style['description']">All my projects are here!</div>
-    <br/><br/>
-    <router-link :to="'/projects/bobconverter'">Linear RGB to sRGB hex converter for Beasts of Bermuda</router-link>
+    <div :class="$style['project-list']">
+        <div :class="$style['project-item']">
+            <a href="https://github.com/WoD5E-Developers/wod5e" target="_blank">
+                <img :src="WOD5EBanner"/>
+            </a>
+        </div>
+        <div :class="$style['project-item']">
+            <a href="https://github.com/Veilza/settlement-sheets" target="_blank">
+                <img :src="SettlementSheetsBanner"/>
+            </a>
+        </div>
+        <div :class="$style['project-item']">
+            <a href="https://github.com/Veilza/show-token-art" target="_blank">
+                <img :src="ShowTokenArtBanner"/>
+            </a>
+        </div>
+        <div :class="$style['project-item']">
+            <div :class="$style['textonly']">
+                <router-link :to="'/projects/bobconverter'">Linear RGB to sRGB hex converter for Beasts of Bermuda</router-link>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style src="./projects.css" module></style>
